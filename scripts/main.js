649f1af5e3c7a02663624be5db858f037a5c3d29
Lauren Miggins <lauren.miggins@gmail.com>
2020-03-26 12:06:42 -0400

    March 23rd upadtes with photos

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,11 @@
-var canvas = document.getElementById("myCanvas");
-var ctx = canvas.getContext("2d");
-function setup() {
-    createCanvas(windowWidth, windowHeight);
-    background("#5db7ff");
-    line ((windowWidth / 2), 0, (windowWidth / 2), 1000);
- }
- function windowResized() {
-    resizeCanvas(windowWidth, windowHeight);
-    background("#5db7ff");
-    line ((windowWidth / 2), 0, (windowWidth / 2), 1000);
- }
+// var x = document.createElement("canvas");
+// function setup() {
+//     createCanvas(windowWidth, windowHeight);
+//     background("#5db7ff");
+//     line ((windowWidth / 2), 0, (windowWidth / 2), 1000);
+//  }
+//  function windowResized() {
+//     resizeCanvas(windowWidth, windowHeight);
+//     background("#5db7ff");
+//     line ((windowWidth / 2), 0, (windowWidth / 2), 1000);
+//  }
